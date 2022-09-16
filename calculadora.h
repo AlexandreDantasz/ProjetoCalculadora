65a67f886ec7ada361e2d6acefda756930796e57
alexandredantas <102476384+AlexandreDantasz@users.noreply.github.com>
2022-09-16 08:09:46 -0300

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -1,11 +1,25 @@
 #ifndef CALCULADORA_H
 #define CALCULADORA_H
 
-
-class Calculadora
-{
-public:
-    Calculadora();
-};
-
+namespace abd{
+    class Calculadora
+    {
+        public:
+            Calculadora();
+            Calculadora(double primeiroValor, double segundoValor);
+            double calcularAdicao()const;
+            double calcularSubtracao()const;
+            double calcularMultiplicacao()const;
+            double calcularDivisao()const;
+            double calcularRadiciacao()const;
+            double calcularPotenciacao()const;
+            void setPrimeiroValor(double newPrimeiroValor);
+            double getPrimeiroValor()const;
+            void setSegundoValor(double newSegundoValor);
+            double getSegundoValor()const;
+        private:
+            double primeiroValor;
+            double segundoValor;
+    };
+}
 #endif // CALCULADORA_H
