88807bd9e196c4fe6b822cb0a425275e9d5209cb
alexandredantas <102476384+AlexandreDantasz@users.noreply.github.com>
2022-09-16 15:53:29 -0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,47 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonInformacoes_clicked();
+
+    void on_pushButtonZero_clicked();
+
+    void on_pushButtonUm_clicked();
+
+    void on_pushButtonDois_clicked();
+
+    void on_pushButtonTres_clicked();
+
+    void on_pushButtonSeis_clicked();
+
+    void on_pushButtonQuatro_clicked();
+
+    void on_pushButtonCinco_clicked();
+
+    void on_pushButtonSete_clicked();
+
+    void on_pushButtonOito_clicked();
+
+    void on_pushButtonNove_clicked();
+
+    void on_pushButtonMultiplicacao_clicked();
+
+    void on_pushButtonSubtracao_clicked();
+
+    void on_pushButtonAdicao_clicked();
+
+    void on_pushButtonParteDecimal_clicked();
+
+    void on_pushButtonPotencializacao_clicked();
+
+    void on_pushButtonRadiciacao_clicked();
+
+    void on_pushButtonDivisao_clicked();
+
+    void on_pushButtonAC_clicked();
+
+    void on_pushButtonApagar_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
