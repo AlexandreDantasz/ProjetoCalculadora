4c184a5828dd323f494529a96bd9f45c2f8b9b0e
alexandredantas <102476384+AlexandreDantasz@users.noreply.github.com>
2022-09-16 15:37:23 -0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <cmath>
+#include <QMessageBox>
+
+QString saida;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +15,137 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButtonInformacoes_clicked()
+{
+    QMessageBox::about(this,"Versão 1.0\n", "Devs\n" "Alexandre Dantas\n" "Bruno Enzo \n" "Diego Silva\n");
+}
+
+
+void MainWindow::on_pushButtonZero_clicked()
+{
+    saida += '0';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonUm_clicked()
+{
+    saida += '1';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonDois_clicked()
+{
+    saida += '2';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonTres_clicked()
+{
+    saida += '3';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonSeis_clicked()
+{
+    saida += '6';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonQuatro_clicked()
+{
+    saida += '4';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonCinco_clicked()
+{
+    saida += '5';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonSete_clicked()
+{
+    saida += '7';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonOito_clicked()
+{
+    saida += '8';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonNove_clicked()
+{
+    saida += '9';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonMultiplicacao_clicked()
+{
+    saida += '*';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonSubtracao_clicked()
+{
+    saida += '-';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonAdicao_clicked()
+{
+    saida += '+';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonParteDecimal_clicked()
+{
+    saida += '.';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonPotencializacao_clicked()
+{
+    saida += '^';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonRadiciacao_clicked()
+{
+    saida += "√";
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonDivisao_clicked()
+{
+    saida += '/';
+    ui->lineEditOperacoes->setText(saida);
+}
+
+
+void MainWindow::on_pushButtonAC_clicked()
+{
+    saida.clear();
+    ui->lineEditOperacoes->setText(saida);
 }
 
